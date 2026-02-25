3fea0f2635883c8a72aa8bc72a4fb50a3fbb26b3
Estevão Antunes <estevao.antunes123@gmail.com>
2026-02-24 21:02:32 -0300

    depois eu faco mai

---
+++ loop/missingLetterDetector/script.js
@@ -0,0 +1,11 @@
+function fearNotLetter(str){
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  for(let letter in alphabet){
+    if(letter.includes(str)){
+      
+    }
+  }
+}
+
+console.log(fearNotLetter("abce"));
